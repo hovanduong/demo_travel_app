f7bc20de78dc4bf81f1b4f90d7de17235c21f440
hovanduong <52810802+hovanduong@users.noreply.github.com>
2021-12-30 14:34:42 +0700

    update tab bar

--- travel_app/lib/src/home/home_screen.dart
+++ travel_app/lib/src/home/home_screen.dart
@@ -16,6 +16,7 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 25),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const SizedBox(height: 30),
               // tieu de
@@ -36,7 +37,23 @@ class _HomeScreenState extends State<HomeScreen> {
                 ],
               ),
               //tab bar
-              
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 20),
+                child: DefaultTabController(
+                    length: 3,
+                    child: TabBar(
+                      indicatorPadding: EdgeInsets.symmetric(horizontal: 25),
+                      isScrollable: true,
+                      labelColor: Colors.blue, // hien thi mau
+                      unselectedLabelColor:
+                          Colors.black, // thay doi mau sau khi nhan
+                      tabs: [
+                        Text('Sights'),
+                        Text('Tours'),
+                        Text('Adventures'),
+                      ],
+                    )),
+              ),
               // danh sach
               ...List.generate(ListTravel.listTravel.length, (index) {
                 return Container(
